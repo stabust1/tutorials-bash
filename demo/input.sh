5ac545d640673eead532ce3893a8958b1989cec0
stabust1 <toanmat12345@gmail.com>
2023-06-26 22:15:20 +0700

    Added input.sh

--- demo/input.sh
+++ demo/input.sh
@@ -4,13 +4,13 @@ echo -e "Xin chào, hãy nhập tên của bạn: "
 read name
 echo "Xin chào, $name!"
 
+echo -e "Hãy nhập tuổi của bạn: "   
+read age
+echo "Tuổi của bạn là: $age"
+
 echo -e "Thành phố của bạn đang sống: "
 read city
 echo "thành phố của bạn đang sống là: $city"
-
-echo -e "Hãy nhập tuổi của bạn: "
-read age
-echo "Tuổi của bạn là: $age"
 
 echo -e "Hãy nhập giới tính của bạn (nam/nữ): "
 read gender
